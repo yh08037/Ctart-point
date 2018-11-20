e218a1a5415ebc72361b4ccbeb6f370496e232e8
yh08037 <44798682+yh08037@users.noreply.github.com>
2018-11-20 20:33:12 +0900

    Add files via upload

---
+++ week6/별찍기.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, j;
+    
+    scanf("%d", &n);
+    
+    for(i=0; i<n; i++){
+        for(j=0; j<n-i; j++){
+            printf("*");
+        }
+        for(j=0; j<i; j++){
+            printf(" ");
+        }
+        for(j=0; j<i+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for(i=0; i<n; i++){
+        for(j=0; j<i; j++){
+            printf(" ");
+        }
+        for(j=0; j<n-i; j++){
+            printf("*");
+        }
+        for(j=0; j<n-i-1; j++){
+            printf(" ");
+        }
+        for(j=0; j<i+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
