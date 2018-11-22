350603b194bac38e69680d3f0c4194ea7ca2f132
yh08037 <44798682+yh08037@users.noreply.github.com>
2018-11-22 13:36:58 +0900

    Create 오류코드.c

---
+++ week2/오류코드.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    char character;
+    
+    scanf("%d", &number);
+    scanf("%c", &character);
+    
+    printf("%d", number);
+    printf("%c", character);
+    
+    return 0;
+}
